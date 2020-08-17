9fd250803dc840135a896854a123557a769cf449
Kyle Hollands <kylehollands@gmail.com>
2020-08-17 16:42:22 -0400

    REASON: Made some minor adjustments to the directories. Specifically where the .txt files reside.

--- run.py
+++ run.py
@@ -21,28 +21,28 @@ class run:
         self.pattern = r'([0-9]{3}[.][a-z][.txt]+)'
         
         # Set origin of reviews in .txt files
-        self.origin = r"C:\Users\KyleHollands\OneDrive\Education\Coursera\Google IT Automation with Python\Automating Real-World Tasks with Python\Week 2\Resources\Process Text Files with Python Dictionaries and Upload to Running Web Service\data\feedback"
-        # origin = r"/data/feedback/"
+        # self.origin = r"C:\Users\KyleHollands\OneDrive\Education\Coursera\Google IT Automation with Python\Automating Real-World Tasks with Python\Week 2\Resources\Process Text Files with Python Dictionaries and Upload to Running Web Service\data\feedback/"
+        self.origin = r"/data/feedback/"
         
     def post_reviews(self):
 
         # Loop through each .txt review and convert it to a dictionary with key, value pairs associated with keys outlined above.
         for review in os.listdir(self.origin):
-            if re.search(self.pattern, review):
-                key_count = 0
-                feedback = {}
+                if re.search(self.pattern, review):
+                    key_count = 0
+                    feedback = {}
                 
-                with open(self.origin + review) as f:
-                    for line in f:
-                        value = line.strip()
-                        feedback[self.keys[key_count]] = value
-                        key_count += 1
-                print(feedback)
+                    with open(self.origin + review) as f:
+                        for line in f:
+                            value = line.strip()
+                            feedback[self.keys[key_count]] = value
+                            key_count += 1
+                    print(feedback)
                 
                 # Post the reviews to the website django API.
-                response = requests.post("http://35.238.230.45/feedback/", json = feedback)
+                response = requests.post("http://35.202.51.144/feedback/", json = feedback)
         
-        print(response.request.body)
-        print(response.status_code)
+        print(Response.request.body)
+        print(Response.status_code)
         
 run().post_reviews()
